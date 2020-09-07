01b05b5fcc636e20f69ab6cdb2b9c551883bfa5c
sweetyicecare <gabrielseedev@outlook.com>
2020-09-07 00:58:51 +0000

    ak3: It's now support for Redmi 3S/P/X
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,19 +4,19 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
+kernel.string=LA.UM.8.6.2.r1/Gabrielse Kernel by Gabrielse @ telegram
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=
-supported.versions=
+do.cleanuponabort=1
+device.name1=land
+supported.versions=10
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
